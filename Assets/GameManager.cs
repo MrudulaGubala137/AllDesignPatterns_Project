26c2970c047c33f269d0b79c7bb905895b9b3247
PTRL480\Vyshnavi.Gubala <vyshnavi.m@yesgnome.com>
2022-04-24 23:57:44 +0530

    Enemy movement towards player and State machine is done.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,14 +5,88 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public enum STATE { LOOKFOR, GOTO, ATTACK, DEAD };
+    public STATE currentState = STATE.LOOKFOR;
+    public float enemySpeed;
+    public float attackDistance;
+    public float gotoDistance;
+    public Transform target;
+    public string playerTag;
+    public float attackTime;
+    public float currentTime;
+    Animator animator;
+   void start()
     {
+        animator = GetComponent<Animator>();
+    }
+    IEnumerator Start()
+    {
+        currentTime = attackTime;
         
+        while (true)
+        {
+            switch (currentState)
+            {
+                case STATE.LOOKFOR:
+                    animator.SetBool("isIdle", true);
+                    LookFor();
+
+                    break;
+                case STATE.GOTO:
+                    animator.SetBool("isRunning", true);
+                    Goto();
+                    break;
+                case STATE.ATTACK:
+                    animator.SetBool("isShooting", true);
+                    Attack();
+                    break;
+                case STATE.DEAD:
+                    animator.SetBool("isSleeping",true);
+                    Dead();
+                    break;
+                default:
+                    break;
+            }
+            yield return null;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LookFor()
+    {
+     
+        if (Vector3.Distance(target.transform.position, this.transform.position) < gotoDistance)
+        {
+            currentState = STATE.GOTO;
+        }
+        print("This is LookForState");
+    }
+    public void Goto()
+    {
+        if (Vector3.Distance(target.transform.position, this.transform.position) > attackDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, enemySpeed * Time.deltaTime);
+        }
+        else
+        {
+            currentState = STATE.ATTACK;
+        }
+        print("This is GotoState");
+    }
+    public void Attack()
     {
         
+        
+        if (Vector3.Distance(target.transform.position, this.transform.position) > attackDistance)
+        {
+            currentState = STATE.ATTACK;
+        }
+        print("This is AttackState");
+    }
+    public void Dead()
+    {
+        print("Game Over!!");
     }
 }
+
+
